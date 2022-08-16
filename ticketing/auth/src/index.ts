5736f4dc55c8fe0c34d85944fa011b154a9c924c
Manel Castro <manel.castro39@gmail.com>
2022-08-16 21:58:08 +0200

    setup ticketing project

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("hi there!");
+});
+
 app.listen(3000, () => {
   console.log("auth listening on port 3000");
 });
