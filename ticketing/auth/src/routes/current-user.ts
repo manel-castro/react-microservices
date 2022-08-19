5791b5ff4b088dbb5d47f37fad90a5302a64e41f
Manel Castro <manel.castro39@gmail.com>
2022-08-19 15:04:37 +0200

    authorizationd and auth middlewares

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,19 +1,17 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import { currentUser } from "../middlewares/current-user";
+import { requireAuth } from "../middlewares/require-auth";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req: Request, res: Response) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
+router.get(
+  "/api/users/currentuser",
+  currentUser,
+  requireAuth,
+  (req: Request, res: Response) => {
+    return res.send({ currentUser: req.currentUser || null });
   }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    return res.send({ currentUser: payload });
-  } catch (err) {
-    return res.send({ currentUser: null });
-  }
-});
+);
 
 export { router as currentUserRouter };
