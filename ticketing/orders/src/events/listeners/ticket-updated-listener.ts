0d47fef9371ce491de655a47826c16cc0a5f114c
Manel Castro <manel.castro39@gmail.com>
2022-11-05 21:54:23 +0100

    feat: setup orders nats

--- ticketing/orders/src/events/listeners/ticket-updated-listener.ts
+++ ticketing/orders/src/events/listeners/ticket-updated-listener.ts
@@ -1,20 +1,24 @@
 import { Subjects, Listener, TicketUpdatedEvent } from "@mcreservations/common";
 import { Message } from "node-nats-streaming";
 import { Ticket } from "../../models/ticket";
+import { natsWrapper } from "../../nats-wrapper";
 import { queueGroupName } from "./queue-group-name";
 
-export class TicketCreatedListener extends Listener<TicketUpdatedEvent> {
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
   queueGroupName: string = queueGroupName;
 
   async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
     const { id, title, price } = data;
 
-    const ticket = Ticket.build({
-      id,
-      title,
-      price,
-    });
+    const ticket = await Ticket.findById(data.id);
+
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    ticket.set({ title, price });
+
     await ticket.save();
 
     msg.ack();
