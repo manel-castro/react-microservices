bf374d7203100e48a72de6184247e608f92df975
mane936 <53373350+mane936@users.noreply.github.com>
2023-01-27 11:17:04 +0100

    add check for completed orders

--- ticketing/orders/src/events/listeners/expiration-completed-listener.ts
+++ ticketing/orders/src/events/listeners/expiration-completed-listener.ts
@@ -18,7 +18,12 @@ export class ExpirationCompletedListener extends Listener<ExpirationCompleteEven
     const order = await Order.findById(data.orderId).populate("ticket");
 
     if (!order) {
+      return;
       throw new Error("Order not found");
+    }
+    if (order.status === OrderStatus.Complete) {
+      return msg.ack();
+      // throw new Error("Order not found");
     }
 
     order.set({
